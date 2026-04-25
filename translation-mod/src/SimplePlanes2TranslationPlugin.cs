d4e0aa7649bf9071beba8fe1ada519db39a2f200
hahaha8459812 <1944523003@qq.com>
2026-04-26 03:32:01 +0800

    Expand Chinese localization coverage for v0.1.1

--- translation-mod/src/SimplePlanes2TranslationPlugin.cs
+++ translation-mod/src/SimplePlanes2TranslationPlugin.cs
@@ -19,7 +19,7 @@ namespace SimplePlanes2TranslationMod
     {
         public const string PluginGuid = "com.codex.simpleplanes2.translation";
         public const string PluginName = "SimplePlanes 2 Translation Mod";
-        public const string PluginVersion = "0.1.0";
+        public const string PluginVersion = "0.1.1";
 
         private const string ManualReloadHotkeyName = "F6";
         private const string ToggleTranslationHotkeyName = "F10";
